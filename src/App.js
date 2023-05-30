d38ae5dfff9fe6e5190cfdefe52e64d9bedf2b0b
justinpsiegel <justinpsiegel@gmail.com>
2023-05-30 12:01:31 -0400

    Updated site to only include complete charts

--- src/App.js
+++ src/App.js
@@ -92,12 +92,6 @@ const App = () => {
                 {/* Charts */}
                 <Route path="/line" element={<Line />} />
                 <Route path="/area" element={<Area />} />
-                <Route path="/bar" element={<Bar />} />
-                <Route path="/pie" element={<Pie />} />
-                <Route path="/financial" element={<Financial />} />
-                <Route path="/color-mapping" element={<ColorMapping />} />
-                <Route path="/pyramid" element={<Pyramid />} />
-                <Route path="/stacked" element={<Stacked />} />
               </Routes>
             </div>
           </div>
